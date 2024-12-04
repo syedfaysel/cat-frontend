39723c2ff302dc6812810fec48f3b21a9516b5d8
Syed Faysel Ahammad Rajo <sfa.rajo20@gmail.com>
2024-12-04 20:25:24 +0600

    frontend project setup ready

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import Test from "@/components/Test"
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <Test></Test>
+      </div>
+    </>
+  )
+}
+
+export default App
